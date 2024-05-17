4417daf883f14b3332bf408e2788387187d0182d
rakibulanas3935 <rakibulanas033@gmail.com>
2024-05-17 12:37:41 +0600

    dynamically get all the value

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('button.json');
+                const jsonData = await response.json();
+                setData(jsonData);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+
     return (
-        <div>Home</div>
+        <div>
+            {data.map(item => (
+                <button
+                    key={item.id}
+                    className="flex-1 px-6 py-3 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300 ease-in-out"
+                >
+                    {item.label}
+                </button>
+            ))}
+        </div>
     )
 }
 
